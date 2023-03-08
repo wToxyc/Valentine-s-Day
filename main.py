0a2c8666250ada4aaa38fcb8511663252dd798a7
BuildTools <unconfigured@null.spigotmc.org>
2023-03-08 22:26:49 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from turtle import *
+
+title("Valentine's Day")
+
+hideturtle()
+
+color("red")
+bgcolor("black")
+fillcolor("red")
+
+begin_fill()
+
+def curve():
+
+    for i in range(200):
+
+        right(1)
+        forward(1)
+
+left(140)
+forward(133)
+
+curve()
+left(120)
+curve()
+
+end_fill()
+
+done()
